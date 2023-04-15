2888d02392758a8ea7f9fcc752aac4e39b62844e
Azrubael <i0638464000@gmail.com>
2023-04-15 13:32:26 +0300

    Step #12. My ist Django project. A simple application 'blog'. More info see corresponding 'README.md'

--- 230414-Django-blog/uhdj1st/blog/admin.py
+++ 230414-Django-blog/uhdj1st/blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    """
+    Мы сообщаем сайту администрирования, что модель зарегистрирована на
+    сайте с использованием конкретно-прикладного класса, который наследует
+    от ModelAdmin. В этот класс можно вставлять информацию о том, как показы-
+    вать модель на сайте и как с ней взаимодействовать.
+    Атрибут list_display позволяет задавать поля модели, которые вы хотите
+    показывать на странице списка объектов администрирования.
+    Декоратор @admin.register() выполняет ту же функцию, что и функция 
+    admin.site.register(), которую заменили, регистрируя декорируемый класс ModelAdmin.
+    """
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+# admin.site.register(Post)
+# Более подробная информация о сайте администрирования находится на
+# странице https://docs.djangoproject.com/en/4.1/ref/contrib/admin/.
--- 230414-Django-blog/uhdj1st/blog/models.py
+++ 230414-Django-blog/uhdj1st/blog/models.py
@@ -8,6 +8,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -30,6 +35,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
             choices=Status.choices,
             default=Status.DRAFT)
+
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() # конкретно- прикладной менеджер 
 
     class Meta:
         """Этот класс определяет метаданные модели.
