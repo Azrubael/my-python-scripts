30afd4c345ac6502476d8218ae82b339f0d9d976
Azrubael <i0638464000@gmail.com>
2024-04-11 19:31:53 +0300

    240323-pwsh-hometasks/home-LAN/cLAN_ping_buntu18.py - Inportant update.

---
+++ 240323-pwsh-hometasks/home-LAN/cLAN_ping_buntu18.py
@@ -0,0 +1,24 @@
+import ipaddress
+import subprocess
+import concurrent.futures
+
+
+network = ipaddress.IPv4Network('192.168.1.0/24')
+
+
+def ping(ip):
+    try:
+        result = subprocess.run(['ping', '-c', '1', str(ip)], stdout=subprocess.DEVNULL, timeout=2)
+        if result.returncode == 0:
+            return (ip, "is up")
+        else:
+            return (ip, "is down")
+    except subprocess.TimeoutExpired:
+        return (ip, "is down")
+
+results = None
+with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
+    results = list(executor.map(ping, network.hosts()))
+    for result in results:
+        if result[1] == "is up": print(result[0], result[1])
+
